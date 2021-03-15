24da5474b0291bd8ab89d8046e2aa5c26816f7a8
Marcus Xavier <59923581+MarcusXavierr@users.noreply.github.com>
2021-03-15 13:26:51 -0300

    Increase timer sleep

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def follow(array_users, driver):
             button = find_follow_button(buttons)
             if button != None:
                 button.click()
-            sleep(1)
+            sleep(2)
         except Exception as e:
             print(e)
             
